136319b4807244da52f8bc4518b1e7a8508d131c
Novah <nour@Nours-MacBook-Pro.local>
2020-06-16 00:04:49 -0700

    scores.cpp

---
+++ scores/GameEntry.cpp
@@ -0,0 +1,20 @@
+#include "GameEntry.h"
+
+
+
+GameEntry::GameEntry(const string& n, int s){
+	name = n;
+	score = s;
+}
+	
+	
+
+
+string GameEntry::getName() const{
+	return name;
+}
+
+
+int GameEntry::getScore() const{
+	return score;
+}
---
+++ scores/main.cpp
@@ -0,0 +1,36 @@
+#include "Score.h"
+#include "GameEntry.h"
+#include "GameEntry.cpp"
+#include "Score.cpp"
+
+
+
+
+int main(){
+
+
+	GameEntry n("novah",150);
+
+	GameEntry z("zaid", 250);
+
+	GameEntry m("mohamed",200);
+
+	Scores sc(3);
+
+	sc.add(n);
+	sc.add(z);
+	sc.add(m);
+
+	for(int i=0; i<3; i++){
+		cout<< sc.entries[i].getName() << endl;
+	}
+
+
+
+
+
+
+
+
+
+}
