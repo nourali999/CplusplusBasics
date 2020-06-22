a9a12a0978250fd3c7a2ace3f7affe77cf977b94
Novah <nour@Nours-MacBook-Pro.local>
2020-06-21 23:08:44 -0700

    Double Pointers

--- game/main.cpp
+++ game/main.cpp
@@ -1,30 +1,47 @@
 #include "game.h"
 
-void insertion(int* A , int n){
-	for (int i = 1; i < n; i++) {
+// void insertion(int* A , int n){
+// 	for (int i = 1; i < n; i++) {
 		
-		int cur = A[i];
-		int j = i - 1;
-		while ((j >= 0) && (A[j] > cur)) {
-			A[j + 1] = A[j];
-			j--;
-		}
-		A[j + 1] = cur;
-		}
-}
-
-
-
-
+// 		int cur = A[i];
+// 		int j = i - 1;
+// 		while ((j >= 0) && (A[j] > cur)) {
+// 			A[j + 1] = A[j];
+// 			j--;
+// 		}
+// 		A[j + 1] = cur;
+// 		}
+// }
 
 
 int main(){
 
 
-	int a[] = {5,4,9,7};
+	int** M = new int*[3];
+	for (int i=0; i<3; i++){
+		M[i] = new int[3];
+	}
 
-	insertion(a,4);
-	cout<< a[0] <<endl <<a[1] << endl<< a[2] << endl << a[3] << endl;
+	int start = 5;
+
+	for(int row = 0; row < 3; row++){
+		cout<<endl;
+		for(int col = 0; col<5; col++){
+			M[row][col] = start;
+			 printf("%-10d ",M[row][col]);
+			start = start + 5;
+		}
+	}
+
+	cout<< endl;
+
+	
+
+
+
+	
+
+	
 
 
 
@@ -32,6 +49,21 @@ int main(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+	// int a[] = {5,4,9,7};
+	// insertion(a,4);
+	// cout<< a[0] <<endl <<a[1] << endl<< a[2] << endl << a[3] << endl;
 	// GameEntry gm("novah",58);
 	// GameEntry n("zaid",100);
 	// n = gm.returnClass()
